5e5e96666e9a71b5d699bf2dff8e4264ef9970fa
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-07-21 18:32:30 +0100

    adding styling to buttons and cards

--- src/components/JokeCard.js
+++ src/components/JokeCard.js
@@ -6,27 +6,35 @@ import '../styles/_jokeCard.scss';
 
 const JokeCard = ({ joke, activeJoke, setActiveJoke }) => {
   const [likeJoke, setLikeJoke] = useState(null);
+  const [activeButton, setActiveButton] = useState(false);
 
   const onLike = (event) => {
     setLikeJoke(event.currentTarget.value);
+    setActiveButton(true);
   };
 
   const onCardClick = () => {
     setActiveJoke(joke);
   };
 
-  const active = joke === activeJoke ? 'active' : '';
+  const activeCard = joke === activeJoke ? 'active' : '';
+  const showLike = likeJoke === 'like' ? 'show' : '';
+  const showFrown = likeJoke === 'dislike' ? 'show' : '';
 
   return (
-    <div className={`joke-card ${active}`} onClick={() => onCardClick()}>
+    <div className={`joke-card ${activeCard}`} onClick={() => onCardClick()}>
+      <div className="card-reverse-liked">
+        <img className={`hidden-smile-${showLike}`} alt="Jokester logo" src={smile} />
+        <img className={`hidden-frown-${showFrown}`} alt="Jokester logo" src={frown} />
+      </div>
       <div className="card-content">
         <div className="card-text">
           <div className="joke-setup">{joke.setup}</div>
           <div className="joke-punchline">{joke.punchline}</div>
         </div>
         <div className="like-buttons">
-          <LikeButton value={"like"} image={smile} onLike={onLike} likeJoke={likeJoke}/>
-          <LikeButton value={"dislike"} image={frown} onLike={onLike} likeJoke={likeJoke}/>
+          <LikeButton value={"like"} image={smile} onLike={onLike} likeJoke={likeJoke} activeButton={activeButton}/>
+          <LikeButton value={"dislike"} image={frown} onLike={onLike} likeJoke={likeJoke} activeButton={activeButton}/>
         </div>
       </div>
     </div>
--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -2,9 +2,8 @@ import React from 'react';
 import '../styles/_likeButton.scss';
 
 const LikeButton = props => {
-  console.log(props.likeJoke)
   return (
-    <button className={`like-button ${props.likeJoke}`} onClick={props.onLike} value={props.value}>
+    <button className={`like-button ${props.likeJoke}${props.value}`} onClick={props.onLike} value={props.value}>
       <img alt={props.value} src={props.image} />
     </button>
   )
